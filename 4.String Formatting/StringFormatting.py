65cc8a56b40f0be46a86c79c93b2862d01de8ed7
shahirulcore <shahirul.amanshah@coreconsulting.asia>
2025-12-04 10:56:08 +0800

    docs: notes on string formatting

--- 4.String Formatting/StringFormatting.py
+++ 4.String Formatting/StringFormatting.py
@@ -2,16 +2,19 @@
 String Formatting
 """
 
-
+# Example 1 (using the f-string)
 first_name = "Eric"
+# Method 1 (using the + operator)
+print("Hello " + first_name)
+# Method 2 (using the f-string, only use the curly braces when we are using the f-string)
+print(f"Hello {first_name}")
 
-sentence = "Hi {} {}"
+# Example 2 (using the format method)
+sentence = "Hi {} {}"  # the order of the placeholder matters because it will be replaced in the order that we pass the arguments
 last_name = "Roby"
-print(sentence.format(first_name, last_name))
+print(
+    sentence.format(first_name, last_name)
+)  # the first_name will be replaced in the first placeholder and the last_name will be replaced in the second placeholder
+print("My name is {} and I am {} years old".format(first_name, 25))
 
 print(f"Hi {first_name} {last_name} I hope you are learning")
-
-
-
-
-
